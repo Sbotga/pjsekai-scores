c665e245db707111cc68342d33ca50efdf8ce455
YumYummity <103061664+YumYummity@users.noreply.github.com>
2024-12-31 17:30:55 -0500

    Update drawing.py

--- src/sekaiworld/scores/drawing.py
+++ src/sekaiworld/scores/drawing.py
@@ -255,7 +255,7 @@ class Drawing:
             ' '.join(filter(lambda x: x, [
                 self.score.meta.difficulty and str(self.score.meta.difficulty).upper(),
                 self.score.meta.playlevel,
-                'Chart by Sbogta'
+                'Chart by Sbotga'
             ])),
             insert=(
                 self.meta_size + self.lane_padding * 4,
